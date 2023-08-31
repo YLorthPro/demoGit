04e0efd2f514640e42479f3755ff4e2071ce1d85
YLorthPro <yann.lorthioir@bstorm.be>
2023-08-31 13:43:23 +0200

    commit clone

--- src/GeoffreyException.java
+++ src/GeoffreyException.java
@@ -1,5 +1,5 @@
 public class GeoffreyException extends RuntimeException{
     public GeoffreyException() {
-        super("Je suis une exception à moi tout seul");
+        super("Je suis une exception à moi tout seul!");
     }
 }
